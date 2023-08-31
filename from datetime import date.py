70e5bccf19f70206a0223d0534f480b22ef46e67
QTmax202 <tuyenksnb202@gmail.com>
2023-08-31 09:33:17 +0700

    datetime_py

---
+++ from datetime import date.py
@@ -0,0 +1,31 @@
+from datetime import date
+from datetime import datetime
+
+today = date.today()
+
+print("Today's date:", today)
+
+# dd/mm/YY
+d1 = today.strftime("%d/%m/%Y")
+print("d1 =", d1)
+
+# Textual month, day and year	
+d2 = today.strftime("%B %d, %Y")
+print("d2 =", d2)
+
+# mm/dd/y
+d3 = today.strftime("%m/%d/%y")
+print("d3 =", d3)
+
+# Month abbreviation, day and year	
+d4 = today.strftime("%b-%d-%Y")
+print("d4 =", d4)
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+print("now =", now)
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
