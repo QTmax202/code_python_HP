597ab15e15180f6cc9871ed3f57d40c328a98b45
QTmax202 <tuyenksnb202@gmail.com>
2023-09-08 14:46:38 +0700

    pdf

--- Crawl data website/import_requests_bs4.py
+++ Crawl data website/import_requests_bs4.py
@@ -4,15 +4,15 @@ response = requests.get("https://tuoitre.vn/tin-moi-nhat.htm")
 
 soup = BeautifulSoup(response.content, "html.parser")
 
-print(soup)
+print("soup",soup)
 
-titles = soup.findAll('h3', class_='title-news')
+titles = soup.findAll('div', class_='box-category-content')
 
 print("titles",titles)
 
 script = soup.findAll('script')
 
-print("script",script)
+# print("script",script)
 
 links = [link.find('a').attrs["href"] for link in titles]
 
